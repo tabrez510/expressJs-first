4b883153cc53cc3961e65588d571ed21d95d2988
tabrez510 <alamtabrez510@gmail.com>
2024-01-15 20:27:42 +0530

    middlewares routers for POST request

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-const http = require('http');
+// const http = require('http');
 
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('first middleware');
-    res.send({ key1: 'value' })
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.use('/add-products', (req, res, next) => {
+    res.send('<form action="/products" method="POST"><input type ="text" name="title" placeholder="Title"><input type="number" name="size" placeholder="size"><button type="submit">Send</button></form>')
     next();
 })
+app.post('/products', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/')
+})
 
-app.use((req, res, next) => {
-    console.log('second middleware');
+app.use('/', (req, res, next) => {
+    res.send('<p>Helllo from express js</p>')
 })
 
 // const server = http.createServer(app);
